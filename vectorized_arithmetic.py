0883a066eb5b1a15c705f398718a45d15926bb54
swacad <swac@Ss-MacBook-Pro.local>
2017-01-08 23:28:22 -0500

    Add plot figures.

--- vectorized_arithmetic.py
+++ vectorized_arithmetic.py
@@ -28,13 +28,33 @@ if __name__ == '__main__':
     for i in range(len(n)):
         l = [x for x in range(n[i])]
         a = np.arange(n[i])
-        list_run_times.append(timeit.timeit('array_add_1d(l, 10)', number=1000, globals=globals()))
-        array_run_times.append(timeit.timeit('a + 10', number=1000, globals=globals()))
+        list_run_times.append(timeit.timeit('array_add_1d(l, 10)', number=10000, globals=globals()))
+        array_run_times.append(timeit.timeit('a + 10', number=10000, globals=globals()))
 
     print(list_run_times)
     print(array_run_times)
 
-    plt.plot(n, list_run_times)
-    plt.plot(n, array_run_times)
-    plt.show()
+    plt.plot(n, list_run_times, label='list run times')
+    plt.plot(n, array_run_times, label='ndarray run times')
+    plt.xlabel('Size of array')
+    plt.ylabel('Run time in seconds for 10000 runs')
+    plt.legend(loc='upper left')
+    plt.savefig('list_v_ndarray.png')
+
+    plt.close()
+    plt.plot(n, array_run_times, label='ndarray run times', color='green')
+    plt.xlabel('Size of array')
+    plt.ylabel('Run time in seconds for 10000 runs')
+    plt.legend(loc='upper left')
+    plt.savefig('ndarray.png')
+
+
+    # plt.plot(x_ticks, in_sample_corr, label='In sample correlation')
+    # plt.plot(x_ticks, out_of_sample_corr, label='Out of sample correlation')
+    # plt.xlabel('Bags')
+    # plt.ylabel('Correlation')
+    # plt.legend(loc='lower right')
+    # plt.title('Bag Learner Correlation: bags=[1...' + str(bags) + '], leaf_size=1')
+    # plt.savefig('bag_corr_red_vary_bags.png')
+    # plt.close()
 
