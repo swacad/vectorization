88c6c3a458647779c785c81014b30b2b66cce8da
swacad <swac@Ss-MacBook-Pro.local>
2017-01-08 23:00:24 -0500

    Added plot generation

--- vectorized_arithmetic.py
+++ vectorized_arithmetic.py
@@ -1,6 +1,7 @@
 import numpy as np
 import copy
 import timeit
+import matplotlib.pyplot as plt
 
 
 def array_add_1d(l, addend):
@@ -19,16 +20,21 @@ def array_add_2d(l, addend):
 
 if __name__ == '__main__':
     # l = [[y + i * 5 for y in range(5)] for i in range(4)]
-    l = [x for x in range(10)]
-    print(l)
-    print(array_add_1d(l, 10))
-    print(l)
 
-    a = np.arange(20).reshape(5, 4)
-    print(a)
-    print(a + 10)
-    a = np.arange(10)
+    n = [x * 1000 for x in range(1, 11)]
+    list_run_times = []
+    array_run_times = []
 
-    print(timeit.timeit('array_add_1d(l, 10)', globals=globals()))
-    print(timeit.timeit('a + 10', globals=globals()))
+    for i in range(len(n)):
+        l = [x for x in range(n[i])]
+        a = np.arange(n[i])
+        list_run_times.append(timeit.timeit('array_add_1d(l, 10)', number=1000, globals=globals()))
+        array_run_times.append(timeit.timeit('a + 10', number=1000, globals=globals()))
+
+    print(list_run_times)
+    print(array_run_times)
+
+    plt.plot(n, list_run_times)
+    plt.plot(n, array_run_times)
+    plt.show()
 
